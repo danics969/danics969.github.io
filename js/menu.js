df7a670840c7b59ccfca58f94e74ab32c0cc317c
Daniel <122456719+danics969@users.noreply.github.com>
2023-03-19 17:32:28 +0100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function showList() {
+    var lista = document.getElementById("miLista");
+    if (lista.style.display === "block") {
+      lista.style.display = "none";
+    } else {
+      lista.style.display = "block";
+    }
+  }
